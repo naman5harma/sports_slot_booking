2d79aa858f52acb1b8607fef40f856a8cd0a244e
ib565 <ish.bhartiya@gmail.com>
2023-11-18 17:18:00 +0530

    Implemented backend logic

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request, render_template, redirect, url_for, session
 from flask_mysqldb import MySQL
+import datetime
 
 app = Flask(__name__)
 
@@ -21,31 +22,47 @@ def list_sports():
     cursor = mysql.connection.cursor()
     cursor.execute("SELECT * FROM sports")
     sports = cursor.fetchall()
-    print(sports)
     cursor.close()
     return render_template('sports.html', sports=sports)
 
-@app.route('/slots/<int:sport_id>')
-def show_slots(sport_id):
-    cursor = mysql.connection.cursor()
-    cursor.execute("SELECT * FROM slots WHERE sport_id = %s", (sport_id,))
-    slots = cursor.fetchall()
-    cursor.close()
-    return render_template('slots.html', slots=slots, sport_id=sport_id)
-
-@app.route('/book')
+@app.route('/book', methods=['GET', 'POST'])
 def display_sports():
+    if request.method == 'POST':
+        sport_id = request.form.get('sport_id')
+        if sport_id:
+            return redirect(url_for('display_slots', sport_id=sport_id))
+
     cursor = mysql.connection.cursor()
     cursor.execute("SELECT * FROM sports")
     sports = cursor.fetchall()
     cursor.close()
     return render_template('book_sport.html', sports=sports)
 
+
+@app.route('/slots/<int:sport_id>')
+def display_slots(sport_id):
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT id, start_time, end_time, slot_type FROM slots WHERE sport_id = %s", (sport_id,))
+    slots = cursor.fetchall()
+    cursor.close()
+    formatted_slots = []
+    for slot in slots:
+        start_time = (datetime.datetime.min + slot[1]).time() 
+        end_time = (datetime.datetime.min + slot[2]).time()  
+        formatted_slot = {
+            "id": slot[0],
+            "start_time": start_time.strftime("%H:%M"), 
+            "end_time": end_time.strftime("%H:%M"),
+            "days": slot[3]
+        }
+        formatted_slots.append(formatted_slot)
+    return render_template('book_slots.html', slots=formatted_slots, sport_id=sport_id)
+
+
 @app.route('/book_slot', methods=['POST'])
 def book_slot():
-    user_id = session.get('user_id', None)  # Ensure the user is logged in
+    user_id = session.get('user_id')
     slot_id = request.form.get('slot_id')
-
     if user_id and slot_id:
         cursor = mysql.connection.cursor()
         try:
@@ -54,21 +71,12 @@ def book_slot():
             message = 'Booking Successful'
         except Exception as e:
             mysql.connection.rollback()
-            message = str(e)
-        cursor.close()
-    else:
-        message = 'Invalid Data'
-
-    return render_template('booking_result.html', message=message)
-
-
-@app.route('/slots/<int:sport_id>')
-def fetch_slots(sport_id):
-    cursor = mysql.connection.cursor()
-    cursor.execute("SELECT id, start_time, end_time FROM slots WHERE sport_id = %s", (sport_id,))
-    slots = cursor.fetchall()
-    cursor.close()
-    return jsonify(slots)
+            message = 'Booking Failed: ' + str(e)
+        finally:
+            cursor.close()
+        return render_template('booking_result.html', message=message)
+    
+    return "Invalid Request", 400
 
 
 @app.route('/register', methods=['GET', 'POST'])
